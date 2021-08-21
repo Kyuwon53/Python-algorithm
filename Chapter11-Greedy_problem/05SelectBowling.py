d010c979ceca5ec3c3e1b438dff60670bad98c19
kyuwon53 <lkw2v@naver.com>
2021-08-22 01:17:11 +0900

    답안 추가

--- Chapter11-Greedy_problem/05SelectBowling.py
+++ Chapter11-Greedy_problem/05SelectBowling.py
@@ -14,3 +14,17 @@ for i in range(m+1):
 result = int(n*(n-1)/2 - cnt)
 print(result)
 
+# 답안
+# 1부터 10까지의 무게를 담을 수 있는 리스트
+array = [0] * 11
+for x in k:
+    # 각 무게에 해당하는 볼링공의 개수 카운트
+    array[x] += 1
+
+result = 0
+# 1부터 m 까지의 각 무게에 대하여 처리
+for i in range(1, m + 1):
+    n -= array[i]   #무게가 i인 볼링공의 개수( A가 선택할 수 있는 개수) 제외
+    result += array[i] * n  #B가 선택하는 경우의 수와 곱하기
+
+print(result)
