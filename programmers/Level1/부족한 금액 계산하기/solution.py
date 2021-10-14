3221edd3428bf52720f00cfe53afb3dcc983f487
kyuwon53 <lkw2v@naver.com>
2021-10-14 23:03:44 +0900

    [Refactoring] 프로그래머스 Lv1 부족한 금액 계산하기 solution

--- programmers/Level1/부족한 금액 계산하기/solution.py
+++ programmers/Level1/부족한 금액 계산하기/solution.py
@@ -4,9 +4,10 @@ def solution(price, money, count):
     # for i in range(1,count+1):
     #     total += price * i
     # answer = total - money
-    answer = price*((1 + count)*count // 2) - money
-    if answer < 0:
-        answer = 0
+    # answer = price*((1 + count)*count // 2) - money
+    # if answer < 0:
+    #     answer = 0
+    answer = max(0,price*((1 + count)*count // 2) - money)
     return answer
 
 print(solution(3,20,4))     # 10
