03addaae3e03f692338c4665a959a2c4a66c06c7
kyuwon53 <lkw2v@naver.com>
2021-10-15 22:21:57 +0900

    [Refactoring] 프로그래머스 Lv1 소수찾기

--- programmers/Level1/소수찾기/solution.py
+++ programmers/Level1/소수찾기/solution.py
@@ -1,13 +1,15 @@
 def solution(n):
     answer = 0
+    a = [ i for i in range(n+1)]
     for i in range(2,n+1):
-        for j in range(2,i+1):
-            if i % j == 0 :
-                if j != i :
-                    break
-                else:
-                    answer += 1
+        if a[i] == 0:
+            continue
+        for j in range( 2 * i, n+1, i ):
+            a[j] = 0
+    for i in range(2, n+1):
+        if a[i] != 0 :
+            answer += 1
     return answer
 
-print(solution(10))
+print(solution(5))
 
