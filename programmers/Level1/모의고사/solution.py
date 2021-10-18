b74fc25fb243c60e7c2ae8774730922b69ec0833
kyuwon53 <lkw2v@naver.com>
2021-10-19 00:53:49 +0900

    [Refactoring] 프로그래머스 모의고사 - solution 완성

--- programmers/Level1/모의고사/solution.py
+++ programmers/Level1/모의고사/solution.py
@@ -3,19 +3,20 @@ def solution(answers):
     one = [1, 2, 3, 4, 5]
     two = [2, 1, 2, 3, 2, 4, 2, 5]
     three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    one_score = 0
-    two_score = 0
-    three_score = 0
+    scores = [0,0,0]
 
     for i, j in enumerate(answers):
         if j == one[i % 5] :
-            one_score += 1
+            scores[0] += 1
         if j == two[i % 8] :
-            two_score += 1
+            scores[1] += 1
         if j == three[i % 10]:
-            three_score += 1
-    # print('one',one_score,'two',two_score,'three',three_score)
+            scores[2] += 1
+
+    for i, score in enumerate(scores):
+        if score == max(scores):
+            answer.append(i+1)
     return answer
 
 
-print(solution([1,3,2,4,2]))
+print(solution([1,2,3,4,5]))
