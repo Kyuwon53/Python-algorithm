8459827dffccf36b6b1667714bc02818c3186d0c
kyuwon53 <lkw2v@naver.com>
2021-11-04 23:41:50 +0900

    [Refactoring] 변수 사용 남발, 불필요한 분기문을 제거하라

--- programmers/Level1/크레인 인형뽑기 게임/solution.py
+++ programmers/Level1/크레인 인형뽑기 게임/solution.py
@@ -3,22 +3,16 @@ def solution(board, moves):
     N = len(board)
     doll = []
     for move in moves:
-        cnt = 0
         for i in range(N):
-            if board[i][move-1] != 0 and cnt == 0:
-                idx = len(doll)
-                if idx != 0:
-                    if doll[idx - 1] == board[i][move - 1]:
-                        doll.pop(idx-1)
-                        answer += 2
-                    else:
-                        doll.append(board[i][move - 1])
-                else:
-                    doll.append(board[i][move-1])
+            if board[i][move-1] != 0:
+                doll.append(board[i][move - 1])
                 board[i][move - 1] = 0
-                cnt += 1
-            else:
-                continue
+                if len(doll) > 1:
+                    if doll[-1] == doll[-2]:
+                        doll.pop(-1)
+                        doll.pop(-1)
+                        answer += 2
+                break
     return answer
 
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
