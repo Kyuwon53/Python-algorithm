3c1238ff79aa861da3ea3cf53da139a170460dc7
kyuwon53 <lkw2v@naver.com>
2022-03-22 23:22:21 +0900

    [수정] 다트 게임 로직 수정 -테스트 통과

--- programmers/Level1/다트 게임/solution.py
+++ programmers/Level1/다트 게임/solution.py
@@ -1,26 +1,41 @@
 def solution(dartResult):
     answer = 0
-    ind = 0
+    scores = []
+    bonus = {
+        'S' : 1,
+        'D' : 2,
+        'T' : 3
+    }
+    before_dart = False
     for dart in dartResult:
-        if dart == 'S':
-            ind = 1
-            answer += pow(num, ind)
-        elif dart == 'D':
-            ind = 2
-            answer += pow(num, ind)
-        elif dart == 'T':
-            ind = 3
-            answer += pow(num, ind)
+        if dart.isdigit():
+            if before_dart:
+                score = scores.pop()
+                dart = score * 10
+            scores.append(int(dart))
+            before_dart = True
+        elif dart.isalpha():
+            score = scores.pop()
+            bonus_score = pow(score, bonus[dart])
+            scores.append(bonus_score)
+            before_dart = False
         elif dart == '*':
-            answer *= 2
+            if len(scores) == 1:
+                current = scores.pop()
+                scores.append(current * 2)
+            else:
+                current = scores.pop()
+                before = scores.pop()
+                scores.append(before * 2)
+                scores.append(current * 2)
+            before_dart = False
         elif dart == '#':
-            answer += pow(num, ind) *(-1) *2
-        elif dart == '0' and num == 1:
-            num *= 10
-        else:
-            num = int(dart)
+            score = scores.pop()
+            scores.append(score * -1)
+            before_dart = False
 
+    answer = sum(scores)
     return answer
 
-dartResult = '1T2D3D#'
+dartResult = '1D2S#10S'
 print(solution(dartResult))
