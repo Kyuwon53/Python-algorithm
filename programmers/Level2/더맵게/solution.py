c713d3cd51b7c5b2061d1f773f4d063246601789
kyuwon53 <lkw2v@naver.com>
2022-03-26 20:01:24 +0900

    [생성] 더 맵게 솔루션 힙큐를 사용한 풀이

--- programmers/Level2/더맵게/solution.py
+++ programmers/Level2/더맵게/solution.py
@@ -1,7 +1,26 @@
+import heapq
+
+
 def solution(scoville, K):
     answer = 0
+    heapq.heapify(scoville)
+    flag = False
+    while len(scoville) >= 1:
+        first = heapq.heappop(scoville)
+        if first > K:
+            flag = True
+            break
+        if len(scoville) >= 1:
+            second = heapq.heappop(scoville)
+            mix = first + (second * 2)
+            heapq.heappush(scoville, mix)
+            answer += 1
+
+    if not flag:
+        answer = -1
     return answer
 
-scoville = [1, 2, 3, 9, 10, 12]
-k = 7
+
+scoville = [5, 3, 9, 10, 12]
+k = 11
 print(solution(scoville, k))
