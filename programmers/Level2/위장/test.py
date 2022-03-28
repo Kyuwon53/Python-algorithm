bf9c5f2a23bd0f0b2d5475382007ef7ce6e06dd6
kyuwon53 <lkw2v@naver.com>
2022-03-29 00:36:56 +0900

    [추가] 프로그래머스 lv2 위장 문제를 풀이 테스트코드 케이스 추가

--- programmers/Level2/위장/test.py
+++ programmers/Level2/위장/test.py
@@ -3,9 +3,10 @@ from solution import solution
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
-        self.assertEqual(solution(clothes), 5)
-
+        clothes1 = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+        self.assertEqual(solution(clothes1), 5)
+        clothes2 = [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+        self.assertEqual(solution(clothes2), 3)
 
 if __name__ == '__main__':
     unittest.main()
