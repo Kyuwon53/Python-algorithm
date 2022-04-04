029c4c9e2e127e173166e6836d11dec9755b73e0
kyuwon53 <lkw2v@naver.com>
2022-04-05 00:50:11 +0900

    [생성] 프로그래머스 lv2 전화번호 목록 문제 풀이

--- programmers/Level2/전화번호 목록/solution.py
+++ programmers/Level2/전화번호 목록/solution.py
@@ -1,3 +1,16 @@
 def solution(phone_book):
     answer = True
+    phone_book.sort()
+    phone = phone_book.pop(0)
+    while phone_book:
+        i = len(phone)
+        if phone == phone_book[0][:i]:
+            answer = False
+            break
+        phone = phone_book.pop(0)
+
     return answer
+
+
+phone_book = ["119", "97674223", "1195524421"]
+print(solution(phone_book))
