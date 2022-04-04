a3a055c3e1b82e4dfd74c2f324150ac7b8e965f5
kyuwon53 <lkw2v@naver.com>
2022-04-05 00:50:29 +0900

    [추가] 프로그래머스 lv2 전화번호 목록 테스트코드

--- programmers/Level2/전화번호 목록/test.py
+++ programmers/Level2/전화번호 목록/test.py
@@ -6,7 +6,11 @@ class MyTestCase(unittest.TestCase):
     def test_1(self):
         phone_book = ["119", "97674223", "1195524421"]
         self.assertEqual(solution(phone_book), False)
-
-
+    def test_2(self):
+        phone_book = ["123","456","789"]
+        self.assertEqual(solution(phone_book), True)
+    def test_3(self):
+        phone_book = ["12","123","1235","567","88"]
+        self.assertEqual(solution(phone_book), False)
 if __name__ == '__main__':
     unittest.main()
