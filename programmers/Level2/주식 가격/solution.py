30032833d47a20e805dd9092675629942bbfe51b
kyuwon53 <lkw2v@naver.com>
2022-04-06 00:39:35 +0900

    [refactoring] deque로 효율성

--- programmers/Level2/주식 가격/solution.py
+++ programmers/Level2/주식 가격/solution.py
@@ -1,3 +1,21 @@
+from collections import deque
+
 def solution(prices):
+    test = deque(prices)
     answer = []
+    for i in range(len(prices)):
+        time = 0
+        test.popleft()
+
+        for t in test:
+            time += 1
+            if prices[i] > t:
+                break
+        answer.append(time)
+
     return answer
+
+
+prices = [5, 8, 6, 2, 4, 1]
+print(solution(prices))
+# [3, 1, 1, 2, 1, 0]
