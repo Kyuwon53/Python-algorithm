0b6cb8eeb5ad1e6a7cd9d444d2e22dc259497b31
kyuwon53 <lkw2v@naver.com>
2022-04-05 23:46:25 +0900

    [refactoring] 리턴값 명시

--- programmers/Level2/전화번호 목록/solution.py
+++ programmers/Level2/전화번호 목록/solution.py
@@ -1,10 +1,8 @@
 def solution(phone_book):
-    answer = True
     phone_book.sort()
     for i in range(len(phone_book) - 1):
         length = len(phone_book[i])
         if phone_book[i] == phone_book[i + 1][:length]:
-            answer = False
-            break
+            return False
 
-    return answer
+    return True
