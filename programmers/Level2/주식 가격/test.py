a04318ba7f11da67289d3f13888c2e3e6dc3c9bf
kyuwon53 <lkw2v@naver.com>
2022-04-06 00:40:26 +0900

    [add] 테스트 케이스 추가

--- programmers/Level2/주식 가격/test.py
+++ programmers/Level2/주식 가격/test.py
@@ -5,7 +5,11 @@ from solution import solution
 class MyTestCase(unittest.TestCase):
     def test_1(self):
         prices = [1, 2, 3, 2, 3]
-        self.assertEqual(solution(prices),[])
+        self.assertEqual(solution(prices), [4, 3, 1, 1, 0])
+
+    def test_2(self):
+        prices = [5, 8, 6, 2, 4, 1]
+        self.assertEqual(solution(prices), [3, 1, 1, 2, 1, 0])
 
 
 if __name__ == '__main__':
