fd04a9862b991f5e181954d23751727184494bfa
kyuwon53 <lkw2v@naver.com>
2022-04-23 17:13:45 +0900

    프로그래머스 lv2 2개 이하로 다른 비트 개선하라

--- programmers/Level2/2개 이하로 다른 비트/solution.py
+++ programmers/Level2/2개 이하로 다른 비트/solution.py
@@ -3,15 +3,19 @@ def solution(numbers):
     # `^` : 두개를 비교하여 다르면 1을 반환
     answer = []
     for num in numbers:
-        flag = True
-        index = 0
-        while flag:
-            index += 1
-            target = int(num) + index
-            count = str(bin(num ^ target)).count('1')
-            if count == 1 or count == 2:
-                flag = False
-        answer.append(target)
+        # 숫자를 4로 나눈 나머지가 3이 아닌 경우 +1
+        if int(num) % 4 != 3:
+            answer.append(num + 1)
+        # 숫자를 4로 나눈 나머지가 3인 경우
+        elif bin(num).count('1') == len(bin(num)[2:]):
+            # 모든 숫자가 1로 이루어진 경우 11 => 101, 111 => 1011, 1111 => 10111
+            result = num + 2 ** (len(bin(num)[2:]) - 1)
+            answer.append(result)
+        else:
+            # 아닌 경우 01 -> 10으로 교체
+            result = int('0b'+(bin(num)[::-1][:-2].replace('10','01',1))[::-1],2)
+            answer.append(result)
+
     return answer
 
-# print(solution([2,7]))
+print(solution([11,10]))
