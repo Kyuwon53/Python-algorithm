f0de01a7964d7caa0a8049a6fa6b5d2fb752b27b
kyuwon53 <lkw2v@naver.com>
2022-06-05 19:30:09 +0900

    테스트 이름을 명확하게 수정하고 케이스 분리

--- programmers/Level2/뉴스 클러스터링/test_solution.py
+++ programmers/Level2/뉴스 클러스터링/test_solution.py
@@ -4,74 +4,90 @@ import solution
 
 
 class Test(TestCase):
-    def test_solution_case1(self):
+    def test_solution_case_normal(self):
         str1 = "FRANCE"
         str2 = "french"
         self.assertEqual(solution.solution(str1, str2), 16384)
 
-    def test_solution_case2(self):
+    def test_solution_case_공백_포함(self):
         str1 = "handshake"
         str2 = "shake hands"
         self.assertEqual(solution.solution(str1, str2), 65536)
 
-    def test_solution_case3(self):
+    def test_solution_case_숫자_특수문자_포함(self):
         str1 = "aa1+aa2"
         str2 = "AAAA12"
         self.assertEqual(solution.solution(str1, str2), 43690)
 
-    def test_solution_case4(self):
+    def test_solution_case_다중집합_존재하지않음(self):
         str1 = "E=M*C^2"
         str2 = "e=m*c^2"
         self.assertEqual(solution.solution(str1, str2), 65536)
 
-    def test_multiset(self):
-        str1 = "FRANCE"
-        str2 = "shake hands"
-        str3 = "aa1+aa2"
-        str4 = "E=M*C^2"
-        str5 = "BAAAA"
-        str6 = "AAA"
+    def test_multiset_case_normal(self):
+        test_string = "FRANCE"
+        self.assertEqual(solution.multi_set(test_string), ["FR", "RA", "AN", "NC", "CE"])
 
-        self.assertEqual(solution.multi_set(str1), ["FR", "RA", "AN", "NC", "CE"])
-        self.assertEqual(solution.multi_set(str2), ["SH", "HA", "AK", "KE", "HA", "AN", "ND", "DS"])
-        self.assertEqual(solution.multi_set(str3), ["AA", "AA"])
-        self.assertEqual(solution.multi_set(str4), [])
-        self.assertEqual(solution.multi_set(str5), ['BA', 'AA', 'AA', 'AA'])
-        self.assertEqual(solution.multi_set(str6), ['AA', 'AA'])
+    def test_multiset_case_공백_포함(self):
+        test_string = "shake hands"
+        self.assertEqual(solution.multi_set(test_string), ["SH", "HA", "AK", "KE", "HA", "AN", "ND", "DS"])
 
-    def test_intersection(self):
+    def test_multiset_case_숫자_특수문자_포함(self):
+        test_string = "aa1+aa2"
+        self.assertEqual(solution.multi_set(test_string), ["AA", "AA"])
+
+    def test_multiset_case_다중집합_존재하지않음(self):
+        test_string = "E=M*C^2"
+        self.assertEqual(solution.multi_set(test_string), [])
+
+    def test_multiset_case_중복된_문자열(self):
+        test_string = "BAAAA"
+        self.assertEqual(solution.multi_set(test_string), ['BA', 'AA', 'AA', 'AA'])
+
+    def test_intersection_normal(self):
         set1 = solution.multi_set("FRANCE")
         set2 = solution.multi_set("french")
 
         self.assertEqual(solution.intersection(set1, set2), ['FR', 'NC'])
 
-        set3 = solution.multi_set("handshake")
-        set4 = solution.multi_set("shake hands")
+    def test_intersection_case_교집합과_다중집합이_같은_경우(self):
+        set1 = solution.multi_set("handshake")
+        set2 = solution.multi_set("shake hands")
 
-        self.assertEqual(solution.intersection(set3, set4), ['HA', 'AN', 'ND', 'DS', 'SH', 'HA', 'AK', 'KE'])
+        self.assertEqual(solution.intersection(set1, set2), ['HA', 'AN', 'ND', 'DS', 'SH', 'HA', 'AK', 'KE'])
 
-        set5 = solution.multi_set("BAAAA")
-        set6 = solution.multi_set("AAA")
+    def test_intersection_case_다른집합이_또다른집합의_부분집합인_경우(self):
+        set1 = solution.multi_set("BAAAA")
+        set2 = solution.multi_set("AAA")
 
-        self.assertEqual(solution.intersection(set5, set6), ['AA', 'AA'])
+        self.assertEqual(solution.intersection(set1, set2), ['AA', 'AA'])
 
-        set7 = solution.multi_set("aa1+aa2")
-        set8 = solution.multi_set("AAAA12")
+    def test_intersection_case_다중집합이_서로_같은_경우(self):
+        set1 = solution.multi_set("aa1+aa2")
+        set2 = solution.multi_set("AAAA12")
 
-        self.assertEqual(solution.intersection(set7, set8), ['AA', 'AA'])
+        self.assertEqual(solution.intersection(set1, set2), ['AA', 'AA'])
 
-    def test_union(self):
+    def test_union_normal(self):
         set1 = solution.multi_set("FRANCE")
         set2 = solution.multi_set("french")
 
         self.assertEqual(solution.union(set1, set2), ['FR', 'RA', 'AN', 'NC', 'CE', 'RE', 'EN', 'CH'])
 
-        set3 = solution.multi_set("handshake")
-        set4 = solution.multi_set("shake hands")
+    def test_union_합집합과_다중집합이_같은_경우(self):
+        set1 = solution.multi_set("handshake")
+        set2 = solution.multi_set("shake hands")
 
-        self.assertEqual(solution.union(set3, set4), ['HA', 'AN', 'ND', 'DS', 'SH', 'HA', 'AK', 'KE'])
+        self.assertEqual(solution.union(set1, set2), ['HA', 'AN', 'ND', 'DS', 'SH', 'HA', 'AK', 'KE'])
 
-        set7 = solution.multi_set("aa1+aa2")
-        set8 = solution.multi_set("AAAA12")
+    def test_union_다른집합이_또다른집합의_부분집합인_경우(self):
+        set1 = solution.multi_set("aa1+aa2")
+        set2 = solution.multi_set("AAAA12")
 
-        self.assertEqual(solution.union(set7, set8), ['AA', 'AA','AA'])
+        self.assertEqual(solution.union(set1, set2), ['AA', 'AA', 'AA'])
+
+    def test_union_case_다중집합이_서로_같은_경우(self):
+        set1 = solution.multi_set("aa1+aa2")
+        set2 = solution.multi_set("AAAA12")
+
+        self.assertEqual(solution.intersection(set1, set2), ['AA', 'AA'])
