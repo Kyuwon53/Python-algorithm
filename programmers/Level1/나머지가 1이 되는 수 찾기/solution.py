e40500f297dd1520fc6a3899032116062b9d7d35
kyuwon53 <lkw2v@naver.com>
2022-06-06 22:25:46 +0900

    문제 의도에 맞게 리팩토링 조건이 나머지가 1이 되도록 하는 가장 작은 자연수이기 때문에 나머지가 1일 때 return

--- programmers/Level1/나머지가 1이 되는 수 찾기/solution.py
+++ programmers/Level1/나머지가 1이 되는 수 찾기/solution.py
@@ -1,9 +1,8 @@
 def solution(n):
-    answer = 0
     for i in range(2, n):
-        if (n-1) % i == 0:
-            answer = i
-            break
-    return answer
+        if n % i == 1:
+            return i
+    return n - 1
+
 
 print(solution(12))
