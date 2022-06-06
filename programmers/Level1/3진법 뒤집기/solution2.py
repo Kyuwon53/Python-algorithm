f4923d7a7b0c1c0ccc5487a120c59cc46bd090f2
kyuwon53 <lkw2v@naver.com>
2022-06-06 22:02:39 +0900

    사용하지 않는 변수를 제거

--- programmers/Level1/3진법 뒤집기/solution2.py
+++ programmers/Level1/3진법 뒤집기/solution2.py
@@ -1,12 +1,9 @@
 def solution(n):
-    answer = 0
     three = ''
     while n > 0:
         three += str(n % 3)
         n //= 3
 
-    answer = int(three,3)
+    answer = int(three, 3)
 
     return answer
-
-print(solution(125))
