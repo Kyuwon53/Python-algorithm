74767e49d4ce8fe44e7c505972526edfdac1b2fc
이규원 <leekyuwon@igyuwon-ui-MacBookAir.local>
2022-10-07 15:23:14 +0900

    구름 챌린지 위클리 1주차 경로의 개수 풀이

---
+++ goorm/challenge/week1/solution1.py
@@ -0,0 +1,18 @@
+"""
+경로의 개수
+i번째 섬에 연결된 모든 다리는 i+1번 섬과 연결되어 있다.
+섬 갯수만큼 다리 개수 입력 받아서 곱하기
+"""
+def solution(n, str):
+    path = list(map(int,str.split()))
+    result = 1
+    for i in range(n):
+        result *= path[i]
+    return result
+
+
+def test_solution():
+    assert solution(3, "2 3 2") == 12
+    assert solution(3, "3 3 3") == 27
+
+test_solution()
